511bbc064182a7d32baaac65828a3768a63137f9
WickedJack99 <wickedjack99.bkp0n@passmail.net>
2025-05-06 15:25:30 +0200

    moin

--- lab07/aaron/e2/upload/run.cpp
+++ lab07/aaron/e2/upload/run.cpp
@@ -1,16 +1,30 @@
-#include "dbscan_parallel.h"
-#include <chrono>
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <chrono>
+#include "dbscan_parallel.h"
 
 using namespace HPC;
+
 
 int main() {
 
   std::vector<Point> points = readPointsFromFile("data");
 
+  // Zeitmessung starten
+  auto start = std::chrono::high_resolution_clock::now();
+
   DBSCAN ds(5, 0.01);
   ds.run(points);
+
+
+  // Zeitmessung beenden
+  auto end = std::chrono::high_resolution_clock::now();
+
+  // Dauer berechnen in Millisekunden
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+  std::cout << "Laufzeit: " << duration << " ms" << std::endl;
+
 
   writePointsToFile(ds.getPoints(), "clustered");
 
