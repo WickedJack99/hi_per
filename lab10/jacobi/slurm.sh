e630a3f225cc6af490c9fb2e440628c76d7d57cd
kai <k.penazzi@web.de>
2025-05-31 13:43:43 +0200

    add slurm file

---
+++ lab10/jacobi/slurm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=G5_jacobi         # Name des Jobs
+#SBATCH --output=jacobi.txt      # Output-Datei (%j = Job-ID)
+#SBATCH --ntasks=48                  # Anzahl Tasks (z. B. MPI-Prozesse)
+#SBATCH --cpus-per-task=1           # Anzahl CPU-Kerne pro Task
+
+# Programm starten
+./a.out
